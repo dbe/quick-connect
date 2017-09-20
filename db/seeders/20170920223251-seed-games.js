eac9764cc19e988f608947b06d64017f09435178
Brian Ethier <davidbrianethier@gmail.com>
2017-09-20 16:40:42 -0600

    Adding seeds and link to preview

---
+++ db/seeders/20170920223251-seed-games.js
@@ -0,0 +1,23 @@
+'use strict';
+var uuidv4 = require('uuid/v4');
+console.log("In seed games")
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Games', [{
+      gameId: uuidv4(),
+      player0Id: uuidv4(),
+      player1Id: uuidv4(),
+      isPlayer0First: true,
+      boardHeights: [8, 8, 8, 8, 8, 8, 8],
+      winCondition: [4],
+      moves: [0, 1, 4, 6, 3, 5, 3, 2],
+      "createdAt": new Date(),
+      "updatedAt": new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Games', null, {});
+  }
+};
